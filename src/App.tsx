3dc4113ecf449f93f1f202f06a0a663e3880096a
xavier-kong <kongxianchengxavier@gmail.com>
2021-11-22 17:50:54 +0800

    added up to video 2: props

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,18 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface AppProps {
+  headerText: string;
+  extraText?: string;
 }
+
+function App({ headerText, extraText }: AppProps) {
+  return (
+    <>
+      <h1>{headerText}</h1>
+      {extraText && <p>{extraText}</p>}
+    </>
+  )
+};
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App headerText="THIS IS A HEADER" extraText="extra text" />
   </React.StrictMode>,
   document.getElementById('root')
 );
