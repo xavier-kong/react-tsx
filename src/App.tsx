096e619a855c2ce5975ce90f40871e3d344474a5
xavier-kong <kongxianchengxavier@gmail.com>
2021-11-22 18:10:20 +0800

    finished up to video 4: forms

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
 import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { User } from './interfaces'
+
 
 function App() {
-  const [ user, setUser ] = useState<User | null>(null);
-
-  const fetchUser = () => setUser({
-    name: 'Mike',
-    age: 24,
-    county: 'No',
-    address: {
-      street: 'main',
-      code: 'none'
-    },
-    admin: false
-  })
 
   return (
     <>
-      <button onClick={fetchUser}>Fetch</button>
-      {user && <p>{user.name}</p>}
     </>
   )
 };
